3e042780f8c63c2fdc79d6ce89c763e619ee0741
gabrielzevedo <gabrielx86@live.com>
2024-05-14 13:12:30 -0300

    chore: fix filename

--- packages/admin-components/src/components/Button/Button.tsx
+++ packages/admin-components/src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import { cva, VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 
-import { Icon, IconProps } from '@/xcomponents/Icon'
+import { Icon, IconProps } from '@/components/Icon'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
